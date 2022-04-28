ca6c28b0da5b531c97794a96b981280d9f155d41
gyro永不抽风 <1247006353@qq.com>
2022-04-28 21:45:13 +0800

    feat: add device option for tests

--- yolo/tests.py
+++ yolo/tests.py
@@ -8,66 +8,77 @@ from utils.visualize import *
 from utils.metrics import *
 
 
-def test_one_batch(net: torch.nn.Module, test_iter_raw: data.DataLoader):
+def test_one_batch(net: torch.nn.Module, test_iter_raw: data.DataLoader, device: torch.device):
 	"""Display result of one test batch
 
 	Args:
 		net (torch.nn.Module): network
 		test_iter_raw (data.DataLoader): test dataloader (raw)
+		device (torch.device): device
 	"""
-	net.eval()
+	with torch.no_grad():
+		net.eval()
+		net.to(device)
 
-	for X, YRaw in test_iter_raw:
-		YHat = net(X)
-		for x, yhat, yraw in zip(X, YHat, YRaw):
-			yhat = nms(yhat)
-			display.display(cv2_to_PIL(draw_detection_result(tensor_to_cv2(x), yhat, raw=False, thres=0.1)))
-			display.display(cv2_to_PIL(draw_ground_truth(tensor_to_cv2(x), yraw)))
-		break
+		for X, YRaw in test_iter_raw:
+			X = X.to(device)
+			YHat = net(X)
+			for x, yhat, yraw in zip(X, YHat, YRaw):
+				yhat = nms(yhat)
+				display.display(cv2_to_PIL(draw_detection_result(tensor_to_cv2(x), yhat, raw=False, thres=0.1)))
+				display.display(cv2_to_PIL(draw_ground_truth(tensor_to_cv2(x), yraw)))
+			break
 
 
-def test_and_draw_mAP(net: torch.nn.Module, test_iter_raw: data.DataLoader):
+def test_and_draw_mAP(net: torch.nn.Module, test_iter_raw: data.DataLoader, device: torch.device):
 	"""Calculate VOCmAP and COCOmAP on test dataset, and draw VOC-AP for each category
 
 	Args:
 		net (torch.nn.Module): network
 		test_iter_raw (data.DataLoader): test dataloader (raw)
+		device (torch.device): device
 	"""
-	net.eval()
+	with torch.no_grad():
+		net.eval()
+		net.to(device)
 
-	# metrics calculation
-	calc = ObjectDetectionMetricsCalculator(20, 0.1)
+		# metrics calculation
+		calc = ObjectDetectionMetricsCalculator(20, 0.1)
 
-	for i, (X, YRaw) in enumerate(test_iter_raw):
-		print("Batch %d / %d" % (i, len(test_iter_raw)))
-		display.clear_output(wait=True)
-		
-		YHat = net(X)
-		for yhat, yraw in zip(YHat, YRaw):
-			yhat = nms(yhat)
-			calc.add_image_data(yhat, yraw)
+		for i, (X, YRaw) in enumerate(test_iter_raw):
+			print("Batch %d / %d" % (i, len(test_iter_raw)))
+			display.clear_output(wait=True)
+			
+			X = X.to(device)
+			YHat = net(X)
+			for yhat, yraw in zip(YHat, YRaw):
+				yhat = nms(yhat)
+				calc.add_image_data(yhat.cpu(), yraw)
 
-	print("Test VOC mAP:", calc.calculate_VOCmAP())
-	print("Test COCO mAP:", calc.calculate_COCOmAP())
+		print("Test VOC mAP:", calc.calculate_VOCmAP())
+		print("Test COCO mAP:", calc.calculate_COCOmAP())
 
-	for i in range(20):
-		draw_precision_recall(calc.calculate_precision_recall(0.5, i), i)
+		for i in range(20):
+			draw_precision_recall(calc.calculate_precision_recall(0.5, i), i)
 
 
-def test_img(net: torch.nn.Module, src: str):
+def test_img(net: torch.nn.Module, src: str, device: torch.device):
 	"""Test an image
 
 	Args:
 		net (torch.nn.Module): network
 		src (str): image path
+		device (torch.device): device
 	"""
-	net.eval()
+	with torch.no_grad():
+		net.eval()
+		net.to(device)
 
-	img = cv2.imread(src)
-	img = torchvision.transforms.functional.resize(cv2_to_PIL(img), (448, 448))
-	to_tensor = torchvision.transforms.ToTensor()
-	X = to_tensor(img).unsqueeze_(0)
-	YHat = net(X)
-	for x, yhat in zip(X, YHat):
-		yhat = nms(yhat)
-		display.display(cv2_to_PIL(draw_detection_result(tensor_to_cv2(x), yhat, raw=False, thres=0.1)))
+		img = cv2.imread(src)
+		img = torchvision.transforms.functional.resize(cv2_to_PIL(img), (448, 448))
+		to_tensor = torchvision.transforms.ToTensor()
+		X = to_tensor(img).unsqueeze_(0).to(device)
+		YHat = net(X)
+		for x, yhat in zip(X, YHat):
+			yhat = nms(yhat)
+			display.display(cv2_to_PIL(draw_detection_result(tensor_to_cv2(x), yhat, raw=False, thres=0.1)))
